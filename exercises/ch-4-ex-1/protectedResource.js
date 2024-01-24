2fa0b4f34758b9d1e54fa0368c39885fc88f325d
Gregg Carey <gregg.carey@outlook.com>
2024-01-24 11:51:36 -0500

    added token verification to resource

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -22,11 +22,43 @@ var resource = {
 	"description": "This data has been protected by OAuth 2.0"
 };
 
-var getAccessToken = function(req, res, next) {
+var getAccessToken = function (req, res, next) {
 	/*
 	 * Scan for an access token on the incoming request.
 	 */
-	
+
+	var inToken = null;
+	const auth = req.headers['authorization'];
+	if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
+		//Prefered method - passing the Auth Token in header
+		inToken = auth.slice('bearer '.length);
+
+	} else if (req.body && req.body.access_Token) {
+		//2nd prefered method - from Form encoded data
+		//Limits API to Form encoded characters 
+		inToken = req.body.access_Token;
+
+	} else if (req.query && req.query.access_Token) {
+		//Least prefered - Query parameter
+		//Tokens can be logged across server requests
+		inToken = req.query.access_Token;
+	}
+
+	nosql.one().make(function (builder) {
+		builder.where('access_token', inToken);
+		builder.callback(function (err, token) {
+			if (token) {
+				console.log(`A token match was found: ${inToken}`);
+			} else {
+				console.log('No token was found');
+			};
+
+			req.access_Token = token;
+			next();
+			return;
+		});
+	});
+
 };
 
 app.options('/resource', cors());
@@ -35,18 +67,24 @@ app.options('/resource', cors());
 /*
  * Add the getAccessToken function to this handler
  */
-app.post("/resource", cors(), function(req, res){
+app.post("/resource", cors(), getAccessToken, function (req, res) {
 
 	/*
 	 * Check to see if the access token was found or not
 	 */
-	
+	if (req.access_Token) {
+		res.json(resource);
+	}
+	else {
+		res.status(401).end();
+	}
+
 });
 
 var server = app.listen(9002, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
+	console.log(`OAuth Resource Server is listening at http://localhost:${port}`);
 });
- 
+
